db666cf9ee58371e005e7f7a95f9111f2301a0ef
hruturaj <hrutunikam007@gmail.com>
2021-03-03 14:41:16 +0530

    brboken our home page into smaller components, finished styling out menu-item component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HomePage } from "./components/homePage/homePage-component.jsx";
+import { HomePage } from "./pages/homePage/homePage-component.jsx";
 
 function App() {
   return (
---
+++ src/pages/homePage/homePage-component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homePage-style.scss";
+
+import { Directory } from "../../components/directory/directory.component.jsx";
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
