03153433c1ac1e9972de1eebf3c3556a753d6872
hruturaj <hrutunikam007@gmail.com>
2021-03-30 21:25:34 +0530

    broken our home page to smaller component, finished styling our menu-item.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import { HomePage } from "./pages/homePage/homePage-component.jsx";
 import { Route, Switch, Link } from "react-router-dom";
+import { ShopPage } from "./pages/shop/shop-component.jsx";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
